f4c3b5bf1e6f84169420562d9ca4e2568c36004f
cedru <klisakek@gmail.com>
2023-09-11 13:50:54 +0300

    add namespace example

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+spl_autoload_register(function ($className) {
+    //var_dump($className);
+    $className = substr($className, 3);
+    require_once __DIR__ . "/../src/$className.php";
+});
+
+use App\Animals\Dog as Koer;
+
+$router = new App\Router();
+var_dump(new Koer());
+
 //var_dump($_SERVER);
 
 switch ($_SERVER['REQUEST_URI']) {
