a956392ccfbd5f486707e98b893544a2afb63f5d
cedru <klisakek@gmail.com>
2023-09-11 13:16:27 +0300

    Add easy server script

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
+
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo 'Home page';
+        break;
+
+    case '/about':
+        echo 'About';
+        break;
+
+    default:
+        echo '404';
+        break;
+}
